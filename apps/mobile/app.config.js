6078dc0f8f8f32ff4196bc7a2f884988938a4545
Thiện Lâm <lamminhthien02012000@gmail.com>
2026-02-07 11:06:09 +0700

    [DGLP-1913] Fix Expo

--- apps/mobile/app.config.js
+++ apps/mobile/app.config.js
@@ -4,13 +4,19 @@ module.exports = ({ config }) => {
   const base = appJson.expo ?? config ?? {};
   const extra = {
     ...(base.extra ?? {}),
-    msTenantId: process.env.EXPO_PUBLIC_MS_TENANT_ID ?? base.extra?.msTenantId ?? "",
-    msClientId: process.env.EXPO_PUBLIC_MS_CLIENT_ID ?? base.extra?.msClientId ?? "",
-    msRedirectUri: process.env.EXPO_PUBLIC_MS_REDIRECT_URI ?? base.extra?.msRedirectUri ?? "",
+    msTenantId:
+      process.env.EXPO_PUBLIC_MS_TENANT_ID ?? base.extra?.msTenantId ?? "",
+    msClientId:
+      process.env.EXPO_PUBLIC_MS_CLIENT_ID ?? base.extra?.msClientId ?? "",
+    msRedirectUri:
+      process.env.EXPO_PUBLIC_MS_REDIRECT_URI ??
+      base.extra?.msRedirectUri ??
+      "",
   };
 
   return {
     ...base,
     extra,
+    plugins: ["expo-router"],
   };
 };
