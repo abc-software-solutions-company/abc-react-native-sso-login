c3112da257f4578cedb7184a1fd30c8c4197ff35
Thiện Lâm <lamminhthien02012000@gmail.com>
2026-02-08 10:07:47 +0700

    Revert "define env"
    This reverts commit 62e877da8023428db2b4fb1434e70136b3a77d29.

--- apps/mobile/app.config.js
+++ apps/mobile/app.config.js
@@ -1,23 +1,15 @@
 const appJson = require("./app.json");
-const envConfig = require("../../../env.json");
 
 module.exports = ({ config }) => {
   const base = appJson.expo ?? config ?? {};
   const extra = {
     ...(base.extra ?? {}),
     msTenantId:
-      process.env.EXPO_PUBLIC_MS_TENANT_ID ??
-      envConfig.EXPO_PUBLIC_MS_TENANT_ID ??
-      base.extra?.msTenantId ??
-      "",
+      process.env.EXPO_PUBLIC_MS_TENANT_ID ?? base.extra?.msTenantId ?? "",
     msClientId:
-      process.env.EXPO_PUBLIC_MS_CLIENT_ID ??
-      envConfig.EXPO_PUBLIC_MS_CLIENT_ID ??
-      base.extra?.msClientId ??
-      "",
+      process.env.EXPO_PUBLIC_MS_CLIENT_ID ?? base.extra?.msClientId ?? "",
     msRedirectUri:
       process.env.EXPO_PUBLIC_MS_REDIRECT_URI ??
-      envConfig.EXPO_PUBLIC_MS_REDIRECT_URI ??
       base.extra?.msRedirectUri ??
       "",
   };
